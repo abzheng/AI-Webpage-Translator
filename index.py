27b6ea67cb36a0f77f62176d7f30b45f6a58931f
abzheng <abzheng07@gmail.com>
2024-09-08 19:41:24 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template, send_file
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 # from io import BytesIO
 import deepl
 # from urllib.parse import urljoin
@@ -9,7 +9,6 @@ import urllib.request
 import os
 import certifi
 from paddleocr import PaddleOCR, draw_ocr
-import time
 
 # fix certificate not found error https://stackoverflow.com/a/73270162/26629340 
 # and PaddleOCR Error #15 https://github.com/PaddlePaddle/PaddleOCR/issues/4613
@@ -40,6 +39,9 @@ def process():
     imageLinks = doc.find_all('amp-img')
     images = []
     textAndCoords = []
+    whitedImage = []
+    finalImages = []
+    groupedOcrData = []
 
     # avoids non-comic images appending to the images list (only testing this script on this one site for now)
     for link in imageLinks:
@@ -57,47 +59,103 @@ def process():
         # remove useless text ^
         if ocrData:
             num = len(ocrData) - 1
-            try:
-                while num != -1:
-                    ocrData[num][1] = list(ocrData[num][1])
 
-                    # if anything in uselessText is found in the string the OCR read, delete it
-                    if any(string in ocrData[num][1][0] for string in uselessText):
-                        del ocrData[num]
+            while num != -1:
+                ocrData[num][1] = list(ocrData[num][1])
 
-                    num -= 1
+                # if anything in uselessText is found in the string the OCR read, delete it
+                if any(string in ocrData[num][1][0] for string in uselessText):
+                    del ocrData[num]
 
-            except IndexError:
-                pass
+                num -= 1
+            
+            # white-ing out the text to prepare it to get drawn over
+            im = Image.open('img.jpg')
+            imgToEdit = ImageDraw.Draw(im)
+            for line in ocrData:
+                # making the area a bit bigger because it usually cuts a bit short
+                xy1 = line[0][0]
+                xy2 = line[0][2]
+                xy1[0] -= 10
+                xy1[1] -= 10
+                xy2[0] += 10
+                xy2[1] += 10
+
+                imgToEdit.rectangle([tuple(xy1), tuple(xy2)], fill ='#FFFFFF')
+                im.save('img.jpg')
+                whitedImage.append(im)
+
+            if len(ocrData) == 1:
+                bottomRight = ocrData[0][0][2]
+                ocrData[0].append(bottomRight)
+                translatedText = translator.translate_text(ocrData[0][1][0], source_lang='ZH', target_lang='EN-US',)
 
         # takes the text, translates it to english, and adds the bounding box + translated text to a list
-        # i have try except for now because i cant figure out whats wrong with the indexing
-            try:
-                for index in range(len(ocrData)):
-                    try:
-                        # while the current line of text's x and y are very close to the next line of text's x and y, group them to make translation more fluid, and stop if they arent
-                        print(ocrData[index + 1][0][0][1], ocrData[index][0][0][1])
-                        while 0 < ocrData[index + 1][0][0][1] - ocrData[index][0][0][1] < 1000 and  -300 < ocrData[index][0][0][0] - ocrData[index + 1][0][0][0] < 300:
-                            ocrData[index + 1][1] = list(ocrData[index + 1][1])
-                            ocrData[index][1][0] += ocrData[index + 1][1][0]
-                            del ocrData[index + 1]
+            else:
+                index = 0
+                while index < len(ocrData) - 1:
+                    currentGroup = ocrData[index]
+                    currentText = currentGroup[1][0]
+                    bottomRight = currentGroup[0][2]
 
-                    except IndexError:
-                        pass
-            except IndexError:
-                pass
+                    # while the current line of text's x and y are very close to the next line of text's x and y, group them to make translation more fluid, and stop if they aren't close
+                    # bounding box is read topleft, topright, bottomright, bottomleft
+                    # ocrData[index][0][0][1] = index's y value
+                    # ocrData[index][0][0][0] = index's x value
+                    while (index < len(ocrData) - 1 and 0 < ocrData[index + 1][0][0][1] - ocrData[index][0][0][1] < 1000 and  -300 < ocrData[index][0][0][0] - ocrData[index + 1][0][0][0] < 300):
+                        nextGroup = ocrData[index + 1]
+                        currentText += nextGroup[1][0]
+                        bottomRight = nextGroup[0][2]
+                        index += 1
 
-            for lis in ocrData:
-                translatedText = translator.translate_text(lis[1][0], source_lang='ZH', target_lang='EN-US',)
-                textAndCoords.append([lis[0][0], translatedText.text])
-        
-        # pseudo code 
-        # use IO paint to white out text on the speech bubbles https://github.com/Sanster/IOPaint
+                    translatedText = translator.translate_text(currentText, source_lang='ZH', target_lang='EN-US')
+                    groupedOcrData.append([currentGroup[0][0], translatedText.text, bottomRight])
+
+                    index += 1
+                    
+                    wordList = translatedText.text.split()
+                    line = []
+                    groupText = []
+                    font = ImageFont.truetype(font='buddychampion.ttf', size=25)
+                    draw = ImageDraw.Draw(whitedImage[0])
+                    # right - left of the original text bbox 
+                    deltaTextLen = groupedOcrData[-1][2][0] - groupedOcrData[-1][0][0]
+                    for word in wordList:
+                        line.append(word)
+                        print(' '.join(line))
+                        # right - left of the line length
+                        deltaLineLen = font.getbbox(' '.join(line))[2] - font.getbbox(' '.join(line))[0]
+                        print(deltaLineLen, '>', deltaTextLen)
+                        if deltaLineLen  > deltaTextLen:
+                            line.insert(-2, '\n')
+                            groupText.append([line])
+                            line = []
+
+        # pseudo code
         # write code to put the text on to the image and then append it to a list
         # return translated images in render_template along with original to toggle on/off
+            # for data in ocrData:
+            #     fontSize = 18
+            #     font = ImageFont.truetype('arial', fontSize)
+            #     draw = ImageDraw.Draw(whitedImage[0])
+            #     xLength = data[0][1][0] - data[0][0][0]
+            #     yLength = data[0][2][1] - data[0][1][1]
+            #     print(xLength, yLength,'\n')
 
-    print (textAndCoords)
-    return render_template('index.html', images=images, textAndCoords=textAndCoords)
+            #     # make a tuple out of this based on the lengths of each side !!!!!!!!!!!!!!!!!!!!!!!!!
+            #     textBbox = draw.textbbox((int((data[0][0][0] + data[0][2][0]) / 2), int((data[0][0][1] + data[0][2][1]) / 2)), translatedText.text, font=font)
+            #     textBboxTuple = (textBbox[2] - textBbox[0], textBbox[3] - textBbox[1])
+            #     while textBboxTuple > (xLength, yLength):
+            #         font = ImageFont.truetype('arial', fontSize)
+            #         fontSize -= 1
+            #         textBbox = draw.textbbox((int((data[0][0][0] + data[0][2][0]) / 2), int((data[0][0][1] + data[0][2][1]) / 2)), translatedText.text, font=font)
+            #         textBboxTuple = (textBbox[2] - textBbox[0], textBbox[3] - textBbox[1])                
+
+                #draw.text(, translatedText.text, tuple(data[0][2]), font)
+
+    print (groupedOcrData)
+    whitedImage[6].show()
+    return render_template('index.html', images=images, textAndCoords=textAndCoords, finalImages=finalImages)
 
 if __name__ == '__main__':
     app.run(debug=True)
